10557fa3554c8cec22a259083ca3087d8b5cb415
jonx1183 <jonx1183@stud.kea.dk>
2023-03-06 11:10:17 +0100

    Added css

--- js/CreateTable.js
+++ js/CreateTable.js
@@ -3,6 +3,20 @@ const tblKommune = document.getElementById("tblKommune");
 
 function createTable(kommune){
     console.log(kommune.navn)
+
+    if(!kommune.kode) return;
+
+    let cellCount = 0;
+    let rowCount = tblKommune.rows.length;
+    let row = tblKommune.insertRow(rowCount);
+    let cell = tblKommune.insertCell(cellCount++);
+    cell.innerHTML = kommune.kode;
+
+    cell = row.insertCell(cellCount++);
+    cell.innerHTML = kommune.navn;
+
+    cell = row.insertCell(cellCount++);
+    cell.innerHTML = kommune.region.navn;
 }
 
 function actionCreateTable(){
