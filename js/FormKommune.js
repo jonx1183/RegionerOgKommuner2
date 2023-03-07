74d1fe33240dc2db318fa43a282bd0b8350eacc4
jonx1183 <jonx1183@stud.kea.dk>
2023-03-07 15:31:36 +0100

    FormKommune Does not work
    Need to add link to photos

--- js/FormKommune.js
+++ js/FormKommune.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', createFormEventListener)
+console.log("Vi er i formkommune")
+
+document.addEventListener('DOMContentLoaded', createFormEventListener);
 let formKommune;
 
 function createFormEventListener(){
@@ -6,24 +8,48 @@ function createFormEventListener(){
     formKommune.addEventListener("submit", handleFormSubmit);
 }
 
-async function handleFormSubmit(event){
+async function handleFormSubmit(event) {
+    //Vi handler submit her, i stedet for default html behaviour
     event.preventDefault();
     const form = event.currentTarget;
     const url = form.action;
     console.log(form)
     console.log(url)
-    console.log(form ===formKommune)
-    try{
-        const formData = new FormData(form);
+    console.log(form === formKommune)
+    try {
+        const formData = new FormData(form)
         console.log(formData)
-        const responseData = await postFormData(url,formData);
-    }catch (error){
+        const responseData = await postFormData(url, formData)
+    } catch (error) {
         alert(error.message)
         console.log(error)
     }
 }
 
-async function postFormData(urlm, formData){
+async function postFormData(url, formData) {
     const plainFormData = Object.fromEntries(formData.entries())
     console.log(plainFormData)
+
+    const region = {}
+    region.kode = "1085"
+    plainFormData.region = region
+
+    const formDataJsonString = JSON.stringify(plainFormData)
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: formDataJsonString
+    }
+
+    const response = await fetch(url, fetchOptions)
+
+    if (!response.ok) {
+        const errorMessage = await response.text()
+        throw new Error(errorMessage)
+    }
+    return response.json();
+
 }
