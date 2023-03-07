ba3909add94bbbe06c982ad63a4fa230d9ef0e8e
jonx1183 <jonx1183@stud.kea.dk>
2023-03-07 13:17:31 +0100

    Added Photos Need to add link to photos

--- js/CreateTable.js
+++ js/CreateTable.js
@@ -17,6 +17,57 @@ function createTable(kommune){
 
     cell = row.insertCell(cellCount++);
     cell.innerHTML = kommune.region.navn;
+
+    cell = row.insertCell(cellCount++);
+    let inpHrefPhoto = document.createElement("input");
+    inpHrefPhoto.type = "text";
+    inpHrefPhoto.setAttribute("value", kommune.hrefPhoto);
+    cell.appendChild(inpHrefPhoto);
+
+    cell = row.insertCell(cellCount++)
+    let img = document.createElement("img")
+    img.setAttribute("src", kommune.hrefPhoto)
+    img.setAttribute("alt", "hej")
+    img.setAttribute("width", 150)
+    img.setAttribute("height", 150)
+    cell.appendChild(img)
+
+    cell = row.insertCell(cellCount++)
+    let pbUpdate = document.createElement("button")
+    pbUpdate.textContent = "Opdater"
+    pbUpdate.className = "buttonupdate"
+    pbUpdate.addEventListener('click', function () {
+        kommune.hrefPhoto = inpHrefPhoto.value;
+        updateKommune(kommune)
+    })
+    cell.appendChild(pbUpdate)
+}
+
+async function updateKommune(kommune) {
+    console.log(kommune.hrefPhoto)
+    console.log(kommune)
+    const response = await restUpdateKommune(kommune)
+    console.log(response)
+}
+
+async function restUpdateKommune(kommune) {
+    const url = "http://localhost:8080/kommune/" + kommune.kode;
+    const fetchOptions = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: ""
+    }
+    const jsonString = JSON.stringify(kommune);
+    fetchOptions.body = jsonString;
+    //calls backend and wait for return
+    const response = await fetch(url, fetchOptions);
+    console.log(response);
+    if (!response.ok) {
+        console.log("Det gik ikke godt med update");
+    };
+    return response;
 }
 
 function actionCreateTable(){
